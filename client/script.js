69a11af4874e764be2af97f538f7205970307902
Manvendra2000 <mzealpro@gmail.com>
2023-02-26 16:08:20 +0530

    update the server fetch

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://ai-codex-rn4e.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
